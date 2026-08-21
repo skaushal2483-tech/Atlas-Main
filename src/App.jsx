6cffc18f4ed6ac38fbf6abd799d1b3350eba0795
skaushal2483-tech <s.kaushal2483@gmail.com>
2026-08-21 15:03:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import DashBoard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <DashBoard />
+  );
+}
+
+export default App;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import "../styles/SideBar.css";
+
+function SideBar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-logo">
+        ATLAS
+      </div>
+
+      <nav className="sidebar-nav">
+        <a className="active">Dashboard</a>
+        <a>Workouts</a>
+        <a>Programs</a>
+        <a>Exercises</a>
+        <a>Progress</a>
+        <a>Membership</a>
+        <a>Settings</a>
+      </nav>
+    </aside>
+  );
+}
+
+export default SideBar;
